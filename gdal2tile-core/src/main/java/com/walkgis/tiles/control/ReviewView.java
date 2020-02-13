5a6ffe175905e3b9155883a8df99981f551d8488
Administrator <xx>
2020-02-13 21:46:08 +0800

    1、修改预览功能 2、修改删除功能 3、修改增加文件功能

--- gdal2tile-core/src/main/java/com/walkgis/tiles/control/ReviewView.java
+++ gdal2tile-core/src/main/java/com/walkgis/tiles/control/ReviewView.java
@@ -3,6 +3,6 @@ package com.walkgis.tiles.control;
 import de.felixroske.jfxsupport.AbstractFxmlView;
 import de.felixroske.jfxsupport.FXMLView;
 
-@FXMLView(value = "/fxml/loading.fxml",title = "预览",bundle = "i18n.index",encoding = "utf-8")
+@FXMLView(value = "/fxml/review.fxml",title = "预览",bundle = "i18n.index",encoding = "utf-8")
 public class ReviewView extends AbstractFxmlView {
 }
